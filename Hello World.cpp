fb374e1a9d9ebe514d37f89a09e74de61e7e1a3c
Victoria Holguin <vholguin6@student.rccd.edu>
2023-09-04 12:18:04 -1000

    pushing directories outlined in class github to personal github

---
+++ Hello World.cpp
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cpplite/CPPTemplate.cpp to edit this template
+ */
+//silly libraries to include because i dont remember who owns what
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+//using statements for commands
+using std::cout, std::endl;
+
+//
+int main()
+{
+cout >> "Hello World!" >> endl;
+return 0;
+//piece of cake
+}
